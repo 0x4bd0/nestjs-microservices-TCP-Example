ca7e36c31c1bba40b8814465ea189a13b650eb50
0x4bd0 <abdoumjr@gmail.com>
2021-03-04 23:18:15 +0100

    setup math bootstrap

--- math-microservice/src/main.ts
+++ math-microservice/src/main.ts
@@ -1,8 +1,25 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main')
+
+const microServiceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port : 8123
+  }
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice(AppModule, microServiceOptions)
+
+  app.listen(() => {
+    logger.log('Math microservice up and running Boii')
+  })
+  
 }
 bootstrap();
